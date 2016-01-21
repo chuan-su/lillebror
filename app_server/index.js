5353c42a09412b50fafff7414e60fbfac3682afa
Chuan Su <chuansu@icloud.com>
2016-01-21 23:16:53 +0100

    initial commit

---
+++ app_server/index.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser');
+
+
+var app = express();
+
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use('/api',require('routes')());
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+
+module.exports = app;
---
+++ app_server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    apiRoutes;
+
+apiRoutes = function(){
+    var router = express.Router();
+
+    return router;
+};
+
+module.exports = apiRoutes;
