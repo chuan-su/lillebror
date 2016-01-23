397e566ecfd2ad61b92d481e9c8cceeae727ced8
Chuan Su <chuansu@icloud.com>
2016-01-23 21:34:54 +0100

    parse xml to json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+'use strict';
+
+var gulp = require('gulp'),
+    Promise = require('bluebird');
 
 gulp.task('default',function(){
 
+});
+
+gulp.task('import',function(){
+    
+    var parser = require('xml2json'),
+        readFile = Promise.promisify(require('fs').readFile);
+        
+    readFile('./data/test.xml','utf8').then(function(dictionary){
+        
+        return Promise.resolve(parser.toJson(dictionary,{object:true}));
+    }).then(function(result){
+        var util = require('util');
+        var words = result.dictionary.word;
+        console.log(util.inspect(words,false,null));
+    }).catch(function(e){
+        console.log("Error",e); 
+    });
 });
