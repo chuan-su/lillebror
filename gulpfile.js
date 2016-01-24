1d55b73486642b4d565161e3aefb0d51a806c4de
Chuan Su <chuansu@icloud.com>
2016-01-24 16:27:06 +0100

    convert the whole xml file to json, not the best solution

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,88 @@ gulp.task('default',function(){
 gulp.task('import',function(){
     
     var parser = require('xml2json'),
-        readFile = Promise.promisify(require('fs').readFile);
+        fs = require('fs'),
+        readFile = Promise.promisify(fs.readFile);
         
-    readFile('./data/test.xml','utf8').then(function(dictionary){
+        
+    readFile('./data/folkets_sv_en_public.xml','utf8').then(function(dictionary){
         
         return Promise.resolve(parser.toJson(dictionary,{object:true}));
     }).then(function(result){
-        var util = require('util');
         var words = result.dictionary.word;
-        console.log(util.inspect(words,false,null));
+
+        require('./app_server/models');
+        var Word = require('./app_server/models/word');
+        var util = require('util');
+        var os = require('os');
+        const wordClass = {
+            
+            nn: "substantiv",
+            jj: "adjektiv",
+            vb: "verb",
+            in: "interjektion",
+            pp: "preposition",
+            pn: "pronomen",
+            ab: "adverb",
+            rg: "grundtal",
+            abbrev: "förkortning"
+        };
+        words.forEach(function(word){
+            
+            var newWord = {
+                value: word.value,
+                lang: word.lang,
+                class: word.class && wordClass[word.class],
+                sound: word.phonetic && word.phonetic.soundFile,
+                pronoun: word.phonetic && word.phonetic.value 
+            };
+
+            if(word.paradigm && word.paradigm.inflection){
+                if(word.paradigm.inflection.constructor == Array){
+                    newWord.inflection = word.paradigm.inflection.map(pair => pair.value);
+                }else{
+                    newWord.inflection = [word.paradigm.inflection.value];
+                }
+            }
+            ['translation','synonym'].forEach(
+                key => {
+                    if (word[key] && word[key].constructor == Array){
+                        newWord[key] = word[key].map(pair => pair.value);
+                    }else if(word[key]){
+                        newWord[key] = [word[key].value];
+                    }
+                }
+            );
+            
+            ['definition','example','idiom','derivation','compund','explanation'].forEach(
+            
+                key => {
+              
+                    if(word[key] && word[key].constructor == Array){
+                        newWord[key] = translationArray(word[key]);
+                      
+                    }else if(word[key]){
+                        
+                        newWord[key]= translationArray([word[key]]);
+                    }
+                }
+            );
+            let wordString = JSON.stringify(newWord);
+            
+            fs.appendFileSync("./data/folkets_sv_en_public.json",wordString+''+os.EOL,'utf8');
+            
+        });
+        console.log('finished');
+        
     }).catch(function(e){
         console.log("Error",e); 
     });
 });
+
+function translationArray(arr){
+    return arr.map(pair => {
+        var newPair = { value: pair.value};
+        if (pair.translation) newPair.translation = pair.translation.value;
+        return newPair;
+    });
+};
