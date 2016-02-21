9909792e53f136eb1ebc6bbb1c35d61db3582a7d
Chuan <chuansu@icloud.com>
2016-02-21 23:51:54 +0100

    use es6 arrow syntax for function

--- app_server/controllers/note.js
+++ app_server/controllers/note.js
@@ -3,45 +3,29 @@ const Note = require('../models/note');
 const elasticsearch = require('../elasticsearch/types/note');
 
 module.exports = {
-    newNote: function(req,res){
+    newNote: (req,res) => {
         var noteToCreate = _.pick(req.body,['body','vocabularies','tags']);
         Note.create(noteToCreate)
-            .then(function(note){
-                res.status(200).json(note).end();
-            })
-            .catch(function(err){
-                res.status(500).json({error: err.message}).end();
-            });
+            .then(note => res.status(200).json(note).end())
+            .catch(err => res.status(500).json({error: err.message}).end());
     },
-    updateNote: function(req,res){
+    updateNote: (req,res) => {
         var id = req.param('id');
         var noteToUpdate = _.pick(req.body,['body','vocabularies','tags']);
         Note.findByIdAndUpdate(id,noteToUpdate,{new: true})
-            .then(function(note){
-                res.status(200).json(note).end();
-            })
-            .catch(function(err){
-                res.status(500).json({error: err.message}).end();
-            });
+            .then(note => res.status(200).json(note).end())
+            .catch(err => res.status(500).json({error: err.message}).end());
     },
-    deleteNote: function(req,res){
+    deleteNote: (req,res) => {
         var id = req.param('id');
         Note.findByIdAndRemove(id,req.body)
-            .then(function(note){
-                res.status(200).json(note).end();
-            })
-            .catch(function(err){
-                res.status(500).json({error: err.message}).end();
-            });
+            .then(note => res.status(200).json(note).end())
+            .catch(err => res.status(500).json({error: err.message}).end());
     },
-    searchNote: function(req,res){
+    searchNote: (req,res) => {
         var query = req.param('verb');
         elasticsearch.search(query)
-            .then(function(notes){
-                res.status(200).json(notes).end();
-            })
-            .catch(function(err){
-                res.status(500).json({error: err.message}).end();
-            });
+            .then(notes => res.status(200).json(notes).end())
+            .catch(err => res.status(500).json({error: err.message}).end());
     }
 };
